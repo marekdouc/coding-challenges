4af780e6c4d863d4617eac4b5bf249d5c9afc476
marekdouc <xenner@seznam.cz>
2023-02-10 12:28:35 +0100

    Challenge 4 is done.

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-const minimumScore = 100;
-
-// Calculate average score.
-function calculateAverageScore(scores) {
-  const total = scores.reduce((acc, currentValue) => acc + currentValue, 0);
-  return total / scores.length;
+// Calculate tip based on the bill.
+function calculateTip(bill) {
+  const range = bill >= 50 && bill <= 300;
+  return range ? bill * (20 / 100) : bill * (15 / 100);
 }
 
-// Average scores.
-const dolphinsAverageScore = calculateAverageScore([96, 108, 89]);
-const koalasAverageScore = calculateAverageScore([88, 91, 110]);
+const bill = 40;
+const tip = calculateTip(bill);
+const totalValue = bill + tip;
 
-// Compare scores.
-const isDraw = dolphinsAverageScore === koalasAverageScore;
-const hasReachedMinimum =
-  dolphinsAverageScore >= minimumScore && koalasAverageScore >= minimumScore;
-const areDolphinsWinners = dolphinsAverageScore > koalasAverageScore;
-const areKoalasWinners = dolphinsAverageScore < koalasAverageScore;
-
-if (!hasReachedMinimum) {
-  console.log(`No one has reached the required minimum score ${minimumScore}.`);
-  console.log(
-    `Dolphins reached average score of ${dolphinsAverageScore.toFixed(
-      2
-    )} and Koalas ${koalasAverageScore.toFixed(2)}`
-  );
-} else {
-  // Check if dolphins are winners.
-  if (areDolphinsWinners) {
-    console.log("Dolphins wins a trophy!");
-  } else if (areKoalasWinners) {
-    console.log("Koalas wins a trophy!");
-  } else {
-    console.log("Ooops.. there is a draw!");
-  }
-}
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value is ${totalValue}.`
+);
